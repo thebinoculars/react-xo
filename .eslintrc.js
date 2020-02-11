c41ddda670bf0edb19a52733c9bd6b13e40c998b
Pham Anh Duc <vndhero@gmail.com>
2020-02-11 22:27:07 +0700

    init commit

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    "react/jsx-filename-extension": [2, { "extensions": [".js", ".jsx"] }],
+    "no-underscore-dangle":  [2, { "allow": ["_id"] }],
+    "indent": ["error", 2],
+    "react/jsx-indent": ["error", 2],
+    "react/jsx-indent-props": ["error", 2],
+    "react/jsx-one-expression-per-line": 1,
+    "react/prop-types": 0,
+    "eqeqeq": 2,
+    "quotes": [2, "single"],
+    "space-unary-ops": 0,
+    "spaced-comment": 0,
+    "strict": 2,
+    "template-curly-spacing": 0,
+    "use-isnan": 2,
+    "valid-jsdoc": 0,
+    "valid-typeof": 2,
+    "vars-on-top": 0,
+  }
+};
---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const cors = require('cors');
+const path = require('path');
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.static('build'));
+
+const PORT = process.env.PORT || 4000;
+
+server.listen(PORT);
+
+const HOST = 1;
+const GUEST = 2;
+
+io.on('connection', (socket) => {
+  socket.on('join', (data) => {
+    const room = io.nsps['/'].adapter.rooms[data.room];
+    if (!room) {
+      socket.join(data.room);
+      io.to(data.room).emit('join', {
+        player: HOST,
+      });
+    }
+    if (room && room.length === 1) {
+      socket.join(data.room);
+      io.to(data.room).emit('join', {
+        player: GUEST,
+      });
+      io.to(data.room).emit('start');
+    }
+  });
+  socket.on('turn', (data) => {
+    io.to(data.room).emit('turn', {
+      room: data.room,
+      turn: data.turn,
+    });
+  });
+  socket.on('play', (data) => {
+    io.to(data.room).emit('play', data);
+  });
+  socket.on('wait', (data) => {
+    io.to(data.room).emit('wait', data);
+    if (!data.wait) {
+      io.to(data.room).emit('start');
+    }
+  });
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(`${__dirname }/build/index.html`));
+});
